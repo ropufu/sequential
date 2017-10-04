21f9f433db4a9a968a0b2cdb661fffc32e065668
Grigory Sokolov <greg.sokolov@gmail.com>
2017-10-04 00:13:49 -0400

    Changed msvc comiler options.

--- hypotheses/Makefile.bat
+++ hypotheses/Makefile.bat
@@ -1,9 +1,6 @@
 @echo off
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-set compilerflags=/Od /Zi /EHsc /I.\..\..\aftermath /I.\..\..\opensource
+set compilerflags=/O2 /W4 /wd4710 /wd4711 /wd4514 /EHsc /I.\..\..\aftermath /I.\..\..\opensource
 set linkerflags=/OUT:hypotheses.exe
 cl.exe %compilerflags% main.cpp /link %linkerflags%
 del main.obj
-del hypotheses.ilk
-del hypotheses.pdb
-del vc140.pdb
